ec65d27ef798baf2ed07fc99b9e4507f836cb73e
Matteo Sbarluzzi <matteosbarluzzi1@gmail.com>
2025-02-26 11:20:28 +0100

    initial commit

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    public function homepage()
+    {
+        $posts = [
+            ['id' => 1, 'title' => 'Esordio col botto', 'content' => 'Questo è il contenuto del primo articolo.'],
+            ['id' => 2, 'title' => 'Promozioni di metà mese', 'content' => 'Questo è il contenuto del secondo articolo.'],
+            ['id' => 3, 'title' => 'Prossime uscite', 'content' => 'Questo è il contenuto del terzo articolo.']
+        ];
+        return view('homepage', compact('posts'));
+    }
+
+    public function chiSiamo()
+    {
+        $team = [
+            ['name' => 'Luca', 'surname' => 'Bianchi', 'role' => 'Fondatore'],
+            ['name' => 'Sara', 'surname' => 'Neri', 'role' => 'Content Creator'],
+            ['name' => 'Marco', 'surname' => 'Verdi', 'role' => 'SEO Specialist']
+        ];
+        return view('chi_siamo', compact('team'));
+    }
+
+    public function chiSiamoDettaglio($name)
+    {
+        $team = [
+            ['name' => 'Luca', 'surname' => 'Bianchi', 'role' => 'Fondatore'],
+            ['name' => 'Sara', 'surname' => 'Neri', 'role' => 'Content Creator'],
+            ['name' => 'Marco', 'surname' => 'Verdi', 'role' => 'SEO Specialist']
+        ];
+
+        foreach ($team as $member) {
+            if ($name == $member['name']) {
+                return view('chi_siamo_dettaglio', compact('member'));
+            }
+        }
+
+        return abort(404);
+    }
+
+    public function servizi()
+    {
+        $services = [
+            ['name' => 'Consulenza SEO', 'description' => 'Ottimizzazione per i motori di ricerca'],
+            ['name' => 'Gestione Social', 'description' => 'Creazione e gestione contenuti'],
+            ['name' => 'Web Design', 'description' => 'Realizzazione di siti web']
+        ];
+        return view('servizi', compact('services'));
+    }
+
+    public function articoli()
+    {
+        $articles = [
+            ['id' => 1, 'title' => 'Esordio col botto', 'content' => 'Questo è il contenuto del primo articolo.'],
+            ['id' => 2, 'title' => 'Promozioni di metà mese', 'content' => 'Questo è il contenuto del secondo articolo.'],
+            ['id' => 3, 'title' => 'Prossime uscite', 'content' => 'Questo è il contenuto del terzo articolo.']
+        ];
+        return view('articoli', compact('articles'));
+    }
+
+    public function articoloDettaglio($id)
+    {
+        $articles = [
+            ['id' => 1, 'title' => 'Esordio col botto', 'content' => 'Questo è il contenuto del primo articolo.'],
+            ['id' => 2, 'title' => 'Promozioni di metà mese', 'content' => 'Questo è il contenuto del secondo articolo.'],
+            ['id' => 3, 'title' => 'Prossime uscite', 'content' => 'Questo è il contenuto del terzo articolo.']
+        ];
+
+        foreach ($articles as $article) {
+            if ($article['id'] == $id) {
+                return view('pagina_dettaglio', compact('article'));
+            }
+        }
+
+        return abort(404);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,69 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BlogController;
 
-Route::get('/', function () {
-    $posts = [
-        ['id' => 1, 'title' => 'Esordio col botto', 'content' => 'Questo è il contenuto del primo articolo.'],
-        ['id' => 2, 'title' => 'Promozioni di metà mese', 'content' => 'Questo è il contenuto del secondo articolo.'],
-        ['id' => 3, 'title' => 'Prossime uscite', 'content' => 'Questo è il contenuto del terzo articolo.']
-    ];
-    return view('homepage', ['posts' => $posts]);
-})->name('homepage');
-
-Route::get('/chi-siamo', function () {
-    $team = [
-        ['name' => 'Luca', 'surname' => 'Bianchi', 'role' => 'Fondatore'],
-        ['name' => 'Sara', 'surname' => 'Neri', 'role' => 'Content Creator'],
-        ['name' => 'Marco', 'surname' => 'Verdi', 'role' => 'SEO Specialist']
-    ];
-    return view('chi_siamo', ['team' => $team]);
-})->name('chi_siamo');
-
-Route::get('/chi-siamo/detail/{name}', function ($name) {
-    $team = [
-        ['name' => 'Luca', 'surname' => 'Bianchi', 'role' => 'Fondatore'],
-        ['name' => 'Sara', 'surname' => 'Neri', 'role' => 'Content Creator'],
-        ['name' => 'Marco', 'surname' => 'Verdi', 'role' => 'SEO Specialist']
-    ];
-    foreach ($team as $member) {
-        if ($name == $member['name']) {
-            return view('chi_siamo_dettaglio', ['member' => $member]);
-        }
-    }
-    return abort(404);
-})->name('chi_siamo_dettaglio');
-
-Route::get('/servizi', function () {
-    $services = [
-        ['name' => 'Consulenza SEO', 'description' => 'Ottimizzazione per i motori di ricerca'],
-        ['name' => 'Gestione Social', 'description' => 'Creazione e gestione contenuti'],
-        ['name' => 'Web Design', 'description' => 'Realizzazione di siti web']
-    ];
-    return view('servizi', ['services' => $services]);
-})->name('servizi');
-
-Route::get('/articoli', function () {
-    $articles = [
-        ['id' => 1, 'title' => 'Esordio col botto', 'content' => 'Questo è il contenuto del primo articolo.'],
-        ['id' => 2, 'title' => 'Promozioni di metà mese', 'content' => 'Questo è il contenuto del secondo articolo.'],
-        ['id' => 3, 'title' => 'Prossime uscite', 'content' => 'Questo è il contenuto del terzo articolo.']
-    ];
-    return view('articoli', ['articles' => $articles]);
-})->name('articoli');
-
-Route::get('/articolo/{id}', function ($id) {
-    $articles = [
-        ['id' => 1, 'title' => 'Esordio col botto', 'content' => 'Questo è il contenuto del primo articolo.'],
-        ['id' => 2, 'title' => 'Promozioni di metà mese', 'content' => 'Questo è il contenuto del secondo articolo.'],
-        ['id' => 3, 'title' => 'Prossime uscite', 'content' => 'Questo è il contenuto del terzo articolo.']
-    ];
-    
-    foreach ($articles as $article) {
-        if ($article['id'] == $id) {
-            return view('pagina_dettaglio', ['article' => $article]);
-        }
-    }
-    
-    return abort(404);
-})->name('articolo_dettaglio');
+Route::get('/', [BlogController::class, 'homepage'])->name('homepage');
+Route::get('/chi-siamo', [BlogController::class, 'chiSiamo'])->name('chi_siamo');
+Route::get('/chi-siamo/detail/{name}', [BlogController::class, 'chiSiamoDettaglio'])->name('chi_siamo_dettaglio');
+Route::get('/servizi', [BlogController::class, 'servizi'])->name('servizi');
+Route::get('/articoli', [BlogController::class, 'articoli'])->name('articoli');
+Route::get('/articolo/{id}', [BlogController::class, 'articoloDettaglio'])->name('articolo_dettaglio');
